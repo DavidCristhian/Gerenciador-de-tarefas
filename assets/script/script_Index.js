7fba5fdf5866cd0603e8065944c0acc96ebf910a
David Cristhian <davidcristhian576@gmail.com>
2025-02-19 20:25:03 -0300

    Organização de pasta, mais inclusão de novas functions

--- assets/script/script_Index.js
+++ assets/script/script_Index.js
@@ -1,33 +1,45 @@
-function addTask(){
+let taskItems = [];
 
+function addTask() {
     let message = document.getElementById("message");
-    let successMessage = "Tarefa adicionada!";
-    let errorMessage = "Campo vazio!, digite uma tarefa para adicioná-la!";
-    let inputElement = document.getElementById("inputTask"); // Captura o elemento HTML
-    let inputTask = inputElement.value.trim(); // Captura o valor do input
+    const successMessage = "Tarefa adicionada!";
+    const errorMessage = "Campo vazio! Digite uma tarefa para adicioná-la!";
 
-    // console.log(inputTask);
+    const inputElement = document.getElementById("inputTask"); // Captura o elemento HTML
+    let inputTask = inputElement.value.trim(); // Captura o valor do input atualizado
 
-    let listTasks = document.getElementById("listTask"); // Captura o elemento HTML
-    let newTask = document.createElement("li"); // Criação de um novo elemento li
-
-    // Verifica se o campo está vazio
-    if(inputTask != ""){
-        newTask.textContent = inputTask;
-
-        listTasks.appendChild(newTask); // Criação de um elemento filho
-        
-        message.classList.remove("errorMessage");
-        message.classList.add("successMesage");
+    if (inputTask !== "") { // Verifica se o campo está vazio
+        message.classList.remove("errorMessage"); 
+        message.classList.add("successMessage");
         message.textContent = successMessage; // Mensagem de sucesso
-    } else{
-        // alert("O campo está vazio!");
 
-        message.classList.remove("successMesage");
+        if (retryCheck(inputTask)) { // Verifica se a tarefa já existe
+            alert("Item já incluso na lista");
+        } else {
+            taskItems.push(inputTask);
+            console.log(taskItems);
+            displayTask();
+        }
+    } else {
+        message.classList.remove("successMessage");
         message.classList.add("errorMessage");
         message.textContent = errorMessage; // Mensagem de erro
     }
 
-    // Reseta o input usando a referência do elemento HTML
-    inputElement.value = "";
+    inputElement.value = ""; // Reseta o input
+}
+
+function displayTask() {
+    const listTasks = document.getElementById("listTask"); // Captura o elemento HTML UL
+    listTasks.innerHTML = ""; // Reseta a lista
+
+    for (let i = 0; i < taskItems.length; i++) {
+        let newTask = document.createElement("li"); // Criação de um novo elemento li
+        newTask.textContent = taskItems[i];
+        listTasks.appendChild(newTask); // Adiciona o elemento à lista
+    }
+}
+
+function retryCheck(task) {
+    return taskItems.includes(task); // Verifica se a tarefa já existe no array
 }
